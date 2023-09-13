1d7743c27bfb2e49c67caa2d60c6d3772942ff06
Tim Wojtulewicz <tim@corelight.com>
2023-09-13 15:13:16 -0700

    Big rework of everything telemetry
    - Reduce code duplication via a set of base classes for Counter/Gauge/Histogram
    - Only create a single otel instrument for each prefix/name combination

--- src/telemetry/Gauge.cc
+++ src/telemetry/Gauge.cc
@@ -6,26 +6,18 @@ using namespace zeek::telemetry;
 
 IntGaugeFamily::IntGaugeFamily(std::string_view prefix, std::string_view name, Span<const std::string_view> labels,
                                std::string_view helptext, std::string_view unit, bool is_sum)
-    : MetricFamily(prefix, name, labels, helptext, unit, is_sum) {}
-
-std::shared_ptr<IntGauge> IntGaugeFamily::GetOrAdd(Span<const LabelView> labels) {
+    : BaseGaugeFamily(prefix, name, labels, helptext, unit, is_sum) {
     auto p = opentelemetry::metrics::Provider::GetMeterProvider();
-    auto m = p->GetMeter(prefix);
-    return std::make_shared<IntGauge>(m->CreateInt64UpDownCounter(prefix + "-" + name, helptext, unit), labels);
+    auto m = p->GetMeter(std::string{prefix});
+    instrument = m->CreateInt64UpDownCounter(std::string{prefix} + "-" + std::string{name}, std::string{helptext},
+                                             std::string{unit});
 }
-
-IntGauge::IntGauge(opentelemetry::nostd::shared_ptr<Handle> hdl, Span<const LabelView> labels) noexcept
-    : hdl(std::move(hdl)), attributes(labels) {}
 
 DblGaugeFamily::DblGaugeFamily(std::string_view prefix, std::string_view name, Span<const std::string_view> labels,
                                std::string_view helptext, std::string_view unit, bool is_sum)
-    : MetricFamily(prefix, name, labels, helptext, unit, is_sum) {}
-
-std::shared_ptr<DblGauge> DblGaugeFamily::GetOrAdd(Span<const LabelView> labels) {
+    : BaseGaugeFamily(prefix, name, labels, helptext, unit, is_sum) {
     auto p = opentelemetry::metrics::Provider::GetMeterProvider();
-    auto m = p->GetMeter(prefix);
-    return std::make_shared<DblGauge>(m->CreateDoubleUpDownCounter(prefix + "-" + name, helptext, unit), labels);
+    auto m = p->GetMeter(std::string{prefix});
+    instrument = m->CreateDoubleUpDownCounter(std::string{prefix} + "-" + std::string{name}, std::string{helptext},
+                                              std::string{unit});
 }
-
-DblGauge::DblGauge(opentelemetry::nostd::shared_ptr<Handle> hdl, Span<const LabelView> labels) noexcept
-    : hdl(std::move(hdl)), attributes(labels) {}
